7b8113b49096850e6e2978e3a191650f8f4c2462
Ferry Ayunda <ayunda.fa0604@gmail.com>
2025-06-21 18:09:46 +0700

    Update app.js

--- public/app.js
+++ public/app.js
@@ -161,7 +161,7 @@ if (withWatermark) {
 tempCtx.fillStyle = "white";
 tempCtx.font = "16px Arial";
 tempCtx.textAlign = "right";
-tempCtx.fillText("#MadeByCIKUPAGES.DEV", canvas.width - 10, canvas.height - 10);
+tempCtx.fillText("#VERDONK", canvas.width - 10, canvas.height - 10);
 }
 return tempCanvas;
 }
