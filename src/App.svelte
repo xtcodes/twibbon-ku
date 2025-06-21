55bf9e560c6d17246ecae29f93476bf7efacc8cd
Ferry Ayunda <ayunda.fa0604@gmail.com>
2025-06-21 14:23:42 +0700

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,37 @@
 <script>
-	export let name;
+export let name;
 </script>
 
 <main>
-	<h1>Twibbon {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<h1>Twibbon {name}!</h1>
+<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+main {
+width: 600px; 
+margin: 5em auto; 
+padding: 2em; 
+background-color: #fdfdff; 
+border-radius: 0.5em; box-shadow: 2px 3px 7px 2px rgba(0,0,0,0.02); 
+} 
+    
+a:link, a:visited { 
+color: #38488f; 
+text-decoration: none; 
+}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+@media (min-width: 600px) {
+.controls {
+flex-direction: row;
+justify-content: center;
+}
+}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+@media (max-width: 700px) { 
+main { 
+margin: 0 auto; 
+width: auto; 
+} 
+}
 </style>
